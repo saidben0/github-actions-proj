6396bc58f2df7259b0351f9a7e2262ee9df1adde
Said Benallal <saidben@amazon.com>
2024-07-26 15:43:06 -0400

    added terraform required version in module/terraform.tf

--- module/terraform.tf
+++ module/terraform.tf
@@ -1,12 +1,17 @@
 terraform {
+  required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source                = "hashicorp/aws"
       configuration_aliases = [aws.acc]
+      version               = "~> 5.60.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = "~> 3.6.2"
     }
   }
+  # required_version = "~> 1.6.2"
+  # required_version = "~> 1.1.9"
 }
