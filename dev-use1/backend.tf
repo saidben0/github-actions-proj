9ddb101ba93fc2123bc7367524d611f82cb3ff0a
Said Benallal <saidben@amazon.com>
2024-07-30 13:04:11 -0400

    testing s3 tfstate

--- dev-use1/backend.tf
+++ dev-use1/backend.tf
@@ -1,4 +1,10 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket         = "enverus-tfstates"
+    key            = "dev/use1/tfstate"
+    region         = "us-east-1"
+    # dynamodb_table = "TerraformStateLocking"
+  }
+  # backend "local" {}
   # required_version = "~> 1.0"
 }
