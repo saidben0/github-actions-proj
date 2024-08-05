b271aef26cd9a5d28328801d7d63ee719f13b3d7
Said Benallal <saidben@amazon.com>
2024-08-05 15:15:11 -0400

    IMPORTANT: replace sqs iam_policy_doc with aws_sqs_queue_policy resource

--- module/iam.tf
+++ module/iam.tf
@@ -158,31 +158,7 @@ resource "aws_iam_role_policy" "sfn_role_policy" {
 }
 
 
-# Allow s3 bucket arn to use s3 notification to trigger sqs queue
-# data "aws_iam_policy_document" "queue" {
-#   provider = aws.acc
-#   statement {
-#     effect = "Allow"
-
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
-
-#     actions   = ["sqs:*"]
-#     # actions   = ["sqs:SendMessage"]
-#     resources = ["arn:aws:sqs:*:*:docs-processing-sqs"]
-#     # resources = [aws_sqs_queue.this.arn]
-
-#     condition {
-#       test     = "ArnEquals"
-#       variable = "aws:SourceArn"
-#       values   = [aws_s3_bucket.this.arn]
-#     }
-#   }
-# }
-
-# Define an S3 bucket policy to allow S3 to send messages to the SQS queue
+# Define an sqs policy to allow S3 to send messages to the SQS queue
 resource "aws_sqs_queue_policy" "this" {
   queue_url = aws_sqs_queue.this.url
 
@@ -194,7 +170,7 @@ resource "aws_sqs_queue_policy" "this" {
         Principal = {
           Service = "s3.amazonaws.com"
         },
-        Action = "SQS:SendMessage",
+        Action = "sqs:SendMessage",
         Resource = aws_sqs_queue.this.arn,
         Condition = {
           ArnEquals = {
@@ -205,22 +181,3 @@ resource "aws_sqs_queue_policy" "this" {
     ]
   })
 }
-
-# resource "aws_sqs_queue" "queue" {
-#   name   = "s3-event-notification-queue"
-#   policy = data.aws_iam_policy_document.queue.json
-# }
-
-# resource "aws_s3_bucket" "bucket" {
-#   bucket = "your-bucket-name"
-# }
-
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.bucket.id
-
-#   queue {
-#     queue_arn     = aws_sqs_queue.queue.arn
-#     events        = ["s3:ObjectCreated:*"]
-#     filter_suffix = ".log"
-#   }
-# }
