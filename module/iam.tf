ba5b6cbbcadb1493c15ffc488f69a76ca3107683
saidben0 <saidben7243@gmail.com>
2024-08-05 09:32:35 -0400

    Update iam.tf

--- module/iam.tf
+++ module/iam.tf
@@ -80,8 +80,10 @@ resource "aws_iam_role_policy" "lambda_sqs_permissions" {
           "sqs:GetQueueAttributes"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:sqs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_sqs_queue.this.name}",
-      "Resource": "arn:aws:sqs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_sqs_queue.dlq.name}"
+      "Resource": [
+          "arn:aws:sqs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_sqs_queue.this.name}",
+          "arn:aws:sqs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_sqs_queue.dlq.name}"
+      ]
     }
   ]
 }
