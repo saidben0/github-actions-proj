676fc350ac4837fa0f8822a3ced4e38c41a6098a
Said Benallal <saidben@amazon.com>
2024-08-07 11:22:32 -0400

    add network stack

--- module/network.tf
+++ module/network.tf
@@ -32,8 +32,8 @@ resource "aws_security_group" "this" {
   # Inbound Rules
   # HTTP access from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
