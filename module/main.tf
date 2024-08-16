a8fa1bafe978e76cc38914967191204576eb2bd7
Said Benallal <saidben@amazon.com>
2024-08-16 18:06:54 -0400

    tf vars updates

--- module/main.tf
+++ module/main.tf
@@ -106,6 +106,8 @@ resource "aws_lambda_event_source_mapping" "this" {
   provider         = aws.acc
   event_source_arn = aws_sqs_queue.this.arn
   function_name    = aws_lambda_function.queue_processing_lambda_function.arn
+  enabled          = true
+  # batch_size       = 1
 }
 
 
