ef14ec5d3abd28baab9da69fda991cc3c34fd601
Said Benallal <saidben@amazon.com>
2024-08-29 17:17:07 -0400

    add bedrock inference config into prompt resource

--- module/bedrock.tf
+++ module/bedrock.tf
@@ -4,8 +4,15 @@ resource "awscc_bedrock_prompt" "this" {
   variants = [
     {
       inference_configuration = {
-        text = {}
+        text = {
+          temperature    = 1
+          top_p          = 0.9900000095367432
+          max_tokens     = 300
+          # stop_sequences = ["\\n\\nHuman:"]
+          top_k          = 250
+        }
       }
+
       name = "variantOne"
       template_configuration = {
         text = {
