1a0e60966df44d0ac3114f2792c300734a05acaf
Said Benallal <saidben@amazon.com>
2024-08-29 13:05:55 -0400

    terraform fmt -recursive

--- dev-use1/main.tf
+++ dev-use1/main.tf
@@ -1,13 +1,13 @@
 module "dev-use1" {
-  source = "../module"
-  inputs_bucket_name = var.inputs_bucket_name
+  source               = "../module"
+  inputs_bucket_name   = var.inputs_bucket_name
   lambda_function_name = var.lambda_function_name
-  lambda_role_name = var.lambda_role_name
-  lambda_policy_name = var.lambda_policy_name
-  dynamodb_table_name = var.dynamodb_table_name
-  prompt_id = var.prompt_id
-  system_prompt_id = var.system_prompt_id
-  system_prompt_ver = var.system_prompt_ver
+  lambda_role_name     = var.lambda_role_name
+  lambda_policy_name   = var.lambda_policy_name
+  dynamodb_table_name  = var.dynamodb_table_name
+  prompt_id            = var.prompt_id
+  system_prompt_id     = var.system_prompt_id
+  system_prompt_ver    = var.system_prompt_ver
 
   providers = {
     aws.acc = aws.use1
