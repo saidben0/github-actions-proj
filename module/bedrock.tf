c8fe7ea17418e71c1f96e9d1c0f7e6a44ce4f635
Said Benallal <saidben@amazon.com>
2024-09-03 09:10:55 -0400

    commenting out inference_configuration of awscc_bedrock_prompt.this tf resource

--- module/bedrock.tf
+++ module/bedrock.tf
@@ -3,15 +3,15 @@ resource "awscc_bedrock_prompt" "this" {
   name            = "${var.prefix}-prompt"
   variants = [
     {
-      inference_configuration = {
-        text = {
-          temperature = 1
-          top_p       = 0.9900000095367432
-          max_tokens  = 300
-          # stop_sequences = ["\\n\\nHuman:"]
-          top_k = 250
-        }
-      }
+      # inference_configuration = {
+      #   text = {
+      #     temperature = 1
+      #     top_p       = 0.9900000095367432
+      #     max_tokens  = 300
+      #     # stop_sequences = ["\\n\\nHuman:"]
+      #     top_k = 250
+      #   }
+      # }
 
       name = "variantOne"
       template_configuration = {
