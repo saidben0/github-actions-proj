079f896ca78751cdf5fcdb537f5801cc44f94b21
Said Benallal <saidben@amazon.com>
2024-09-05 12:21:59 -0400

    deploy a test prompt

--- module/bedrock.tf
+++ module/bedrock.tf
@@ -1,5 +1,5 @@
 resource "awscc_bedrock_prompt" "this" {
-  # provider        = awscc.acc
+  provider        = awscc.acc
   default_variant = "variantOne"
   name            = "${var.prefix}-prompt"
   variants = [
@@ -7,10 +7,9 @@ resource "awscc_bedrock_prompt" "this" {
       inference_configuration = {
         text = {
           temperature    = 0
-          top_p          = 0.9900000095367432
-          max_tokens     = 300
-          stop_sequences = ["\\n\\nHuman:"]
-          top_k          = 250
+          # top_p          = 0.9900000095367432
+          # max_tokens     = 300
+          # top_k          = 250
         }
       }
 
@@ -28,44 +27,7 @@ resource "awscc_bedrock_prompt" "this" {
 }
 
 
-resource "awscc_bedrock_prompt_version" "this" {
-  # provider   = awscc.acc
-  prompt_arn = awscc_bedrock_prompt.this.arn
-}
-
-
-#### TEST PROMPT ####
-resource "awscc_bedrock_prompt" "test" {
-  # provider        = awscc.acc
-  default_variant = "variantOne"
-  name            = "${var.prefix}-prompt-test"
-  variants = [
-    {
-      inference_configuration = {
-        text = {
-          temperature = 0
-          top_p       = 0.9900000095367432
-          max_tokens  = 300
-          # stop_sequences = ["\\n\\nHuman:"]
-          top_k = 250
-        }
-      }
-
-      name = "variantOne"
-      template_configuration = {
-        text = {
-          text = file("${path.module}/templates/prompt_template_test.txt")
-        }
-      }
-      template_type = "TEXT"
-    },
-  ]
-
-  tags = var.tags
-}
-
-
-resource "awscc_bedrock_prompt_version" "test" {
-  # provider   = awscc.acc
-  prompt_arn = awscc_bedrock_prompt.test.arn
-}
+# resource "awscc_bedrock_prompt_version" "this" {
+#   provider   = awscc.acc
+#   prompt_arn = awscc_bedrock_prompt.this.arn
+# }
