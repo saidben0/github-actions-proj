8c71cafbbe0d5e3cda661cd2db5d03b0e8830a26
Said Benallal <saidben@amazon.com>
2024-09-05 11:45:54 -0400

    deploy a test prompt

--- module/bedrock.tf
+++ module/bedrock.tf
@@ -31,5 +31,38 @@ resource "awscc_bedrock_prompt" "this" {
 
 resource "awscc_bedrock_prompt_version" "this" {
   provider   = awscc.acc
-  prompt_arn = awscc_bedrock_prompt.this.arn
+  prompt_arn = awscc_bedrock_prompt.test.arn
+}
+
+
+#### TEST PROMPT ####
+resource "awscc_bedrock_prompt" "test" {
+  provider        = awscc.acc
+  default_variant = "variantOne"
+  name            = "${var.prefix}-prompt-test"
+  variants = [
+    {
+      inference_configuration = {
+        text = {
+          temperature = 0
+        }
+      }
+
+      name = "variantOne"
+      template_configuration = {
+        text = {
+          text = file("${path.module}/templates/prompt_template.txt")
+        }
+      }
+      template_type = "TEXT"
+    },
+  ]
+
+  tags = var.tags
+}
+
+
+resource "awscc_bedrock_prompt_version" "test" {
+  provider   = awscc.acc
+  prompt_arn = awscc_bedrock_prompt.test.arn
 }
