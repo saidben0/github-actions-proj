3d8b9237f72fbed60e7a60df767152021c35d7ca
Said Benallal <saidben@amazon.com>
2024-09-06 11:48:14 -0400

    troubleshooting s3 state access denied error

--- dev-use1/backend.tf
+++ dev-use1/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "di-dev-terraform"
+    bucket = "di-dev-terraform/dev/llandman/"
     # bucket = "enverus-tfstates-0823" # for testing in proserve shared acc
-    key    = "dev/llandman/terraform.tfstate"
+    key    = "terraform.tfstate"
     # key    = "dev/use1/tfstate" # for testing in proserve shared acc
     region = "us-east-1"
   }
