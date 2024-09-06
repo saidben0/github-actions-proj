9ac45124b332e409aaf1ce88e782aa6736b95e7d
Said Benallal <saidben@amazon.com>
2024-09-06 11:50:41 -0400

    terraform fmt -recursive

--- dev-use1/backend.tf
+++ dev-use1/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "di-dev-terraform"
     prefix = "dev/llandman"
     # bucket = "enverus-tfstates-0823" # for testing in proserve shared acc
-    key    = "terraform.tfstate"
+    key = "terraform.tfstate"
     # key    = "dev/use1/tfstate" # for testing in proserve shared acc
     region = "us-east-1"
   }
