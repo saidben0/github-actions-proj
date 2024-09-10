642158fb819e9ee9ee529913cb2ba08dd6d64cc2
Said Benallal <saidben@amazon.com>
2024-09-10 10:19:32 -0400

    change dlq from standard to .fifo

--- module/main.tf
+++ module/main.tf
@@ -29,7 +29,8 @@ resource "random_id" "this" {
 
 resource "aws_sqs_queue" "dlq" {
   provider = aws.acc
-  name     = "${var.prefix}-dlq"
+  name     = "${var.prefix}-dlq.fifo"
+  fifo_queue = true
   # kms_master_key_id = data.aws_kms_key.this.id
 }
 
