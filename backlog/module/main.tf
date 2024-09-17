ec88d8d010980d253d64796076bd49f8c51d8a0a
Said Benallal <saidben@amazon.com>
2024-09-17 16:48:09 -0400

    test prompt versioning

--- backlog/module/main.tf
+++ backlog/module/main.tf
@@ -27,60 +27,12 @@ resource "random_id" "this" {
 }
 
 
-# resource "aws_sqs_queue" "dlq" {
-#   provider = aws.acc
-#   name     = "${var.prefix}-dlq"
-#   # fifo_queue = true
-#   # kms_master_key_id = data.aws_kms_key.this.id
-# }
-
 resource "aws_sqs_queue" "redrive_dlq" {
   provider   = aws.acc
   name       = "${var.prefix}-redrive-dlq.fifo"
   fifo_queue = true
-  # kms_master_key_id = data.aws_kms_key.this.id
 }
 
-
-# resource "null_resource" "lambda_layer" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       pwd && ls
-#       cd ../module
-#       mkdir -p ./lambda-layer/python
-#       pip install -r ./lambda-layer/requirements.txt --platform=manylinux2014_x86_64 --only-binary=:all: -t ./lambda-layer/python
-#       # rm ./lambda-layer/requirements.txt
-#     EOT
-#   }
-
-#   triggers = {
-#     filebasesha = "${base64sha256(file("${path.module}/lambda-layer/requirements.txt"))}"
-#   }
-#   # triggers = {
-#   #   always_run = "${timestamp()}"
-#   # }
-# }
-
-# # Package the Lambda Layer
-# data "archive_file" "lambda_layer" {
-#   type        = "zip"
-#   output_path = "${path.module}/lambda-layer.zip"
-
-#   source_dir = "${path.module}/lambda-layer"
-#   excludes   = ["requirements.txt"]
-
-#   depends_on = [null_resource.lambda_layer]
-# }
-
-# # Create Lambda Layer
-# resource "aws_lambda_layer_version" "lambda_layer" {
-#   layer_name          = "python-libs"
-#   description         = "Lambda layer for Land Llandman doc processing"
-#   compatible_runtimes = ["python${var.python_version}"]
-#   filename            = data.archive_file.lambda_layer.output_path
-#   # filename            = "${path.module}/lambda-layer/python-libs.zip"
-#   source_code_hash = data.archive_file.lambda_layer.output_base64sha256
-# }
 
 # Package the Lambda function code
 data "archive_file" "this" {
@@ -90,30 +42,6 @@ data "archive_file" "this" {
   output_path = "${path.module}/outputs/lambda-artifacts.zip"
 }
 
-# resource "aws_lambda_function" "queue_processing_lambda_function" {
-#   provider      = aws.acc
-#   filename      = data.archive_file.this.output_path
-#   function_name = "${var.prefix}-${var.lambda_function_name}"
-#   role          = data.aws_iam_role.llandman_lambda_exec_role.arn
-#   # layers                         = [aws_lambda_layer_version.lambda_layer.arn]
-#   handler                        = "lambda_handler.lambda_handler"
-#   source_code_hash               = data.archive_file.this.output_base64sha256
-#   runtime                        = "python${var.python_version}"
-#   timeout                        = "900"
-#   reserved_concurrent_executions = 100
-#   memory_size                    = 1024
-
-#   environment {
-#     variables = {
-#       DDB_TABLE_NAME = aws_dynamodb_table.model_outputs.name
-#       QUEUE_URL      = aws_sqs_queue.this.url
-#     }
-#   }
-
-#   tracing_config {
-#     mode = "Active"
-#   }
-# }
 
 resource "aws_lambda_function" "invoke_model_lambda_function" {
   provider      = aws.acc
@@ -234,21 +162,6 @@ resource "aws_sqs_queue_policy" "this" {
   })
 }
 
-# # send an s3 event to sqs when new s3 object is created/uploaded
-# resource "aws_s3_bucket_notification" "sqs_notification" {
-#   provider = aws.acc
-#   bucket   = data.aws_s3_bucket.inputs_bucket.id
-
-#   queue {
-#     queue_arn = aws_sqs_queue.this.arn
-#     events    = ["s3:ObjectCreated:*"]
-#     # filter_prefix = aws_s3_object.inputs.key
-#     # filter_suffix = ".pdf"
-#   }
-
-#   depends_on = [aws_sqs_queue_policy.this]
-# }
-
 # map sqs queue to trigger the lambda function when an 3 event is received
 resource "aws_lambda_event_source_mapping" "this" {
   provider         = aws.acc
@@ -277,17 +190,8 @@ resource "aws_dynamodb_table" "model_outputs" {
     type = "S"
   }
 
-  # attribute {
-  #   name = "chunk_id"
-  #   type = "N"
-  # }
-
   point_in_time_recovery {
     enabled = true
   }
 
-  # server_side_encryption {
-  #   enabled     = true
-  #   kms_key_arn = data.aws_kms_key.this.arn
-  # }
 }
