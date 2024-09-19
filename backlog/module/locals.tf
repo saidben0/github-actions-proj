491ab556448a0aa9b24ad0549e98008ce7f5b14f
Said Benallal <saidben@amazon.com>
2024-09-19 09:38:50 -0400

    terraform fmt -recursive

--- backlog/module/locals.tf
+++ backlog/module/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  account_id       = data.aws_caller_identity.this.account_id
-  partition        = data.aws_partition.this.partition
-  region           = data.aws_region.this.name
+  account_id = data.aws_caller_identity.this.account_id
+  partition  = data.aws_partition.this.partition
+  region     = data.aws_region.this.name
 
   # prompt_id        = awscc_bedrock_prompt.this["mainPrompt"].prompt_id
   # system_prompt_id = awscc_bedrock_prompt.this["systemPrompt"].prompt_id
