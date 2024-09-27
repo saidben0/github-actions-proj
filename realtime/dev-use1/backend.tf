c248b57fa683f8e578394ba48ec51cbcf4000344
Said Benallal <saidben@amazon.com>
2024-09-27 14:55:59 -0400

    update tf backend for batch pipeline

--- realtime/dev-use1/backend.tf
+++ realtime/dev-use1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    # bucket = "di-dev-terraform"
-    bucket = "enverus-tfstates-0823" # for testing in proserve shared acc
+    bucket = "di-dev-terraform"
+    # bucket = "enverus-tfstates-0823" # for testing in proserve shared acc
     key    = "dev/llandman/terraform.tfstate"
     region = "us-east-1"
   }
